39ab938a0a57238a8b64d9c690e022fdb6c5dc26
Tanishi <tanishi.janweja@gmail.com>
2025-04-17 11:36:35 +0530

    Re-added backend1 as a regular directory

---
+++ backend1/dist/Game.js
@@ -0,0 +1,69 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Game = void 0;
+const chess_js_1 = require("chess.js");
+const messages_1 = require("./messages");
+class Game {
+    constructor(player1, player2) {
+        this.moveCount = 0;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = new chess_js_1.Chess();
+        this.startTime = new Date();
+        this.player1.send(JSON.stringify({
+            type: "INIT_GAME",
+            payload: {
+                color: "white",
+            },
+        }));
+        this.player2.send(JSON.stringify({
+            type: "INIT_GAME",
+            payload: {
+                color: "black",
+            },
+        }));
+    }
+    makeMove(socket, move) {
+        if (this.moveCount % 2 === 0 && socket !== this.player1) {
+            return;
+        }
+        if (this.moveCount % 2 === 1 && socket !== this.player2) {
+            return;
+        }
+        try {
+            this.board.move(move);
+        }
+        catch (e) {
+            return;
+        }
+        if (this.board.isGameOver()) {
+            this.player1.emit(JSON.stringify({
+                type: messages_1.GAME_OVER,
+                payload: {
+                    winner: this.board.turn() === "w" ? "black" : "white",
+                },
+            }));
+            this.player2.emit(JSON.stringify({
+                type: messages_1.GAME_OVER,
+                payload: {
+                    winner: this.board.turn() === "w" ? "black" : "white",
+                },
+            }));
+            return;
+        }
+        if (this.moveCount % 2 === 0) {
+            this.player2.send(JSON.stringify({
+                type: messages_1.MOVE,
+                payload: move,
+            }));
+        }
+        else {
+            this.player1.send(JSON.stringify({
+                type: messages_1.MOVE,
+                payload: move,
+            }));
+        }
+        this.moveCount++;
+    }
+}
+exports.Game = Game;
